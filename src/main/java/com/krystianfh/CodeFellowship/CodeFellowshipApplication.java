1ad9761dfa6d930a131d2afef38cd936373a11f4
KrystianFH <krystianfrancuz@gmail.com>
2020-10-12 08:58:39 -0700

    refactored package names for main and test app

--- src/main/java/com/krystianfh/CodeFellowship/CodeFellowshipApplication.java
+++ src/main/java/com/krystianfh/CodeFellowship/CodeFellowshipApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodeFellowshipApplication {
+public class codefellowshipApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodeFellowshipApplication.class, args);
+		SpringApplication.run(codefellowshipApplication.class, args);
 	}
 
 }
